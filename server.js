09c44ecac129607a4aea194fce59b7ae4b40b070
Boris Djukic <boris.djukic@lanaco.com>
2019-12-01 22:12:44 +0100

    služenje statičkog sadržaja sa express bibliotekom je jednostavnije

--- server.js
+++ server.js
@@ -1,88 +1,10 @@
-const http = require("http");
-const path = require("path");
-const fs = require("fs");
+const express = require('express')
+
+const port = 3000
+const app = express()
 
 
-
-const server = http.createServer((req, res) => {
-
-  // Build file path
-  let filePath = path.join(
-    __dirname,
-    req.url === "/" ? "index.html"  : req.url
-  );
-
-  // Extension of file
-  let extname = path.extname(filePath);
-
-  // Initial content type
-  let contentType = "text/html";
-
-  // Check ext and set content type
-  switch (extname) {
-    case ".js":
-      contentType = "text/javascript";
-      break;
-    case ".css":
-      contentType = "text/css";
-      break;
-    case ".json":
-      contentType = "application/json";
-      break;
-    case ".png":
-      contentType = "image/png";
-      break;
-    case ".jpg":
-      contentType = "image/jpg";
-      break;
-     // case ".php":
-      //contentType = "PHP Source File";
-     // break;
-  }
-
-  // Check if contentType is text/html but no .html file extension
-  if (contentType == "text/html" && extname == "") filePath += ".html";
-
-  // log the filePath
-  console.log(filePath);
-
-  // Read File
-  fs.readFile(filePath, (err, content) => {
-    if (err) {
-      if (err.code == "ENOENT") {
-        // Page not found
-        fs.readFile(
-          path.join(__dirname, "404.html"),
-          (err, content) => {
-            res.writeHead(200, { "Content-Type": "text/html" });
-            res.end(content, "utf8");
-          }
-        );
-      } else {
-        //  Some server error
-        res.writeHead(500);
-        res.end(`Server Error: ${err.code}`);
-      }
-    } else {
-      // Success
-      res.writeHead(200, { "Content-Type": contentType });
-      res.end(content, "utf8");
-    }
-  });
-});
+app.use(express.static('public'))
 
 
-
-//Write file                                 <=       OVDE JE PROBLEM
-/* let data = NESTO STO TREBA DA STIGNE OVDJE SA FAJLA script.js (VALJDA).
-let json = JSON.stringify(data);
-fs.writeFile('words1.json', json, error => {
-  if (error) throw error;
-  console.log('Rijeci su unesene');
-});
-*/
-const PORT = process.env.PORT || 3000;
-
-server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
-
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
